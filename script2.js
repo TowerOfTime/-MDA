6f979e61b4ea86e9c734bbc6c819e736df2cf49b
Anna_Willien <128370283+TowerOfTime@users.noreply.github.com>
2024-05-16 01:19:22 +0800

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,3 +1,20 @@
+const form = document.querySelector("form"),
+        nextBtn = form.querySelector(".nextBtn"),
+        backBtn = form.querySelector(".backBtn"),
+        allInput = form.querySelectorAll(".first input");
+
+
+nextBtn.addEventListener("click", ()=> {
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
+        }
+    })
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 25,
